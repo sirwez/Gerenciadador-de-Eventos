f47cd7cbd07683b5f145429e2322cae2ee673495
Weslley Aquino <48765861+sirwez@users.noreply.github.com>
2021-06-23 19:24:23 -0300

    COMEÇO DE UMA NOVA ERA
    Co-Authored-By: Gustavo André <55256750+tiratreco@users.noreply.github.com>

---
+++ salasAluguel/especialidades.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Cadastro de Especialidade</title>
+    <meta charset="utf-8">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php require("navbar.php"); ?>
+
+    <div class="container my-5">
+      <h1>Cadastro de Especialidade</h1>
+    </div>
+    <div class="container mt-5">
+	<form action="php_action/createEspecialidade.php" method="POST">
+        <div class="form-group">
+		<label for="especialidade">Especialidade</label>
+		<input class="form-control" type="text" name="especialidade" id="especialidade">
+
+        </div>
+        <div  class="text-center">
+		<button type="submit"  class="btn btn-outline-secondary mt-5 mx-auto" name="btn-cadastrar"> Criar </button>
+        </div>
+        
+      </form>
+    </div>
+  </body>
+</html>
---
+++ salasAluguel/index.php
@@ -0,0 +1,11 @@
+<?php
+include_once 'php_action/db_connect.php';
+include_once 'includes/header.php';
+include_once 'includes/message.php';
+?>
+
+<a href="especialidades.php" class="btn">Adicionar cliente</a> </div>
+
+<?php
+include_once 'includes/footer.php';
+?>
