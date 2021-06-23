ded671ef52e0ebcf557c15b12d457b4b718083d1
gustavo <tiratreco@gmail.com>
2021-06-23 20:50:54 -0300

    a

--- salasAluguel/interfaces/visualizar_especialidades.php
+++ salasAluguel/interfaces/visualizar_especialidades.php
@@ -14,8 +14,7 @@
       <h1>Lista de Especialidades</h1>
     </div>
     <div class="container mt-5">
-      
-<<<<<<< HEAD
+
     <div class="card mx-auto text-center" style="width: 25rem;">
     <ul class="list-group list-group-flush">
         <li class="list-group-item">Especialidade_1</li>
@@ -23,69 +22,5 @@
         <li class="list-group-item">Especialidade_1</li>
     </ul>
     </div>
-
-=======
-    <table class="table table-hover text-center">
-    <thead>
-        <tr>
-        <th scope="col">Especialidades</th>
-        </tr>
-    </thead>
-    <tbody>
-<<<<<<< HEAD
-        <tr>
-        <td scope="row">Especialidade_1</td>
-        </tr>
-        <tr>
-        <td scope="row">Especialidade_2</td>
-        </tr>
-        <tr>
-        <td scope="row">Especialidade_3</td>
-        </tr>
-    </tbody>
-    </table>
->>>>>>> main
-    </div>
-  </body>
-</html>
-
-<table class="striped">
-				<thead>
-					<th>Nome:</th>
-					<th>Sobrenome:</th>
-					<th>Email:</th>
-					<th>Idade:</th>
-				</thead>
-				<tbody>
-					<?php 
-				$sql = "SELECT * FROM clientes";
-=======
-    <?php 
-				$sql = "SELECT * FROM especialidade";
->>>>>>> main
-				$result = mysqli_query($connect, $sql);
-
-				if(mysqli_num_rows($result)>0):
-
-				while($dados = mysqli_fetch_array($result)):
-			?>
-        <tr>
-        <td scope="row">Especialidade: <?php echo $dados['nome'];?></td>
-        </tr>
-    </tbody>
-    <?php 
- endwhile;
-			else:?>
-							<tr>
-								<td>-</td>
-								<td>-</td>
-								<td>-</td>
-								<td>-</td>
-							</tr>
-							<?php
-endif;
-?>
-    </table>
-    </div>
-  </body>
-</html>
+</div>
+</body>
